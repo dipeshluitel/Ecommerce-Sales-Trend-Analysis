8add7d817944c5d6c16de6d86f0f8eda5493dec1
Dipesh <dipeshluitel40@gmail.com>
2025-11-11 01:00:17 +0545

    convert the data types

--- SalesDataAnalysis/main.py
+++ SalesDataAnalysis/main.py
@@ -1,4 +1,20 @@
 import pandas as pd 
+import numpy as np
 df = pd.read_csv("sampleData/Superstore.csv", encoding="latin1")
+df['Order ID'] = df["Order ID"].astype("string")
+df['Order Date'] = pd.to_datetime(df["Order Date"])
+df['Ship Date'] = pd.to_datetime(df["Ship Date"])
+df['Customer ID'] = df['Customer ID'].astype("string")
+df['Customer Name'] = df["Customer Name"].astype("string")
+df['Country'] = df["Country"].astype("string")
+df['City'] = df["City"].astype("string")
+df['State'] = df["State"].astype("string")
+df['Postal Code'] = df["Postal Code"].astype("string")
+df['Region'] = df["Region"].astype("string")
+df['Product ID'] = df["Product ID"].astype("string")
+df['Category'] = df["Category"].astype("string")
+df['Sub-Category'] = df["Sub-Category"].astype("string")
+df['Product Name'] = df["Product Name"].astype("string")
 
-print(df.isnull().sum())
+
+print(df.info())
