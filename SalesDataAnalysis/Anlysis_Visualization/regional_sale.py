35efcff2055fed2d42c0254231c8c7f892a206ee
Dipesh <dipeshluitel40@gmail.com>
2025-11-11 16:00:26 +0545

    US Regional sales analyzed

---
+++ SalesDataAnalysis/Anlysis_Visualization/regional_sale.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from KPIs import df
+
+regional_sales = df.groupby('Region')[['Sales','Profit']].sum().sort_values('Sales',ascending=False)
+
+print(regional_sales)
+
+# Plot
+
+plt.figure(figsize=(10,5))
+sns.barplot(data=regional_sales,x='Sales',y='Region',orient='h')
+plt.title("Regional Sales Comparision")
+plt.tight_layout()
+plt.show()
