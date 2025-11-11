dc8bcfefd42397fe1c25332e828b6853660992ed
Dipesh <dipeshluitel40@gmail.com>
2025-11-11 14:20:06 +0545

    Top 10 products of all time by sales listed

---
+++ SalesDataAnalysis/Anlysis_Visualization/top_product.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from KPIs import df
+
+#  TOP 10 best selling products
+
+top_product = df.groupby('Product Name')[['Sales','Profit']].sum().sort_values('Sales', ascending=False).head(10)
+top_product.reset_index(inplace=True)
+
+print(top_product)
+
+#  Plot
+
+plt.figure(figsize=(10,5))
+sns.barplot(data=top_product, x='Sales', y='Product Name', orient='h')
+plt.title("Top 10 Products of all time by Sales")
+plt.tight_layout()
+plt.show()
